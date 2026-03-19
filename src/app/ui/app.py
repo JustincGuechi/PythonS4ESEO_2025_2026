deeada36f478ca8f25866f34cd5bbd70fc960354
NlsHub <nilscoudry@gmail.com>
2026-03-19 08:50:16 +0100

    Update app.py

--- src/app/ui/app.py
+++ src/app/ui/app.py
@@ -167,38 +167,7 @@ class GraphExplorerApp:
     def add_edge(self):
         """Ajoute une arête au graphe (via dialogue)."""
         # TODO: implémenter
-        u=simpledialog.askstring("Arête","Nœud de départ: ",parent=self.root)
-        if not u: 
-            return 
-        
-        v=simpledialog.askstring("Arête","Nœud d'arrivée: ",parent=self.root)
-        if not v: 
-            return 
 
-        if u in self.graph.nodes() and v in self.graph.nodes():
-            self.graph.add_edge(u, v)
-            self.draw_graph()
-            print(f"Arête ajoutée entre {u} et {v}")
-        else:
-            from tkinter import messagebox
-            messagebox.showerror("Erreur","L'un des nœuds n'existe pas.")
-        depart = simpledialog.askstring("Nouvelle Arête", "Entrez le nom du nœud de départ:")
-        if not depart or not depart.strip():
-            return 
-            depart = depart.strip
-
-        arrivee = simpledialog.askstring("Nouvelle Arête", "Entrez le nom du nœud d'arrivée':")
-        if not arrivee or not arrivee.strip():
-            return 
-            arrivee = arrivee.strip
-
-        try :
-            self.graph.add_edge(depart, arrivee)
-            self.statusVariable.set(f"Arête ajoutée avec succès : {source} -> {cible}")
-
-        except Exception as e:
-            messagebox.showerror("Erreur d'ajout", "Impossible de créer l'arête")
-    
         u = simpledialog.askstring("Arête", "Nœud de départ :", parent=self.root)
         if not u or not u.strip(): 
             return 
@@ -218,7 +187,7 @@ class GraphExplorerApp:
                 messagebox.showerror("Erreur d'ajout", f"Impossible de créer l'arête :\n{e}")
         else:
             messagebox.showerror("Erreur", "L'un des nœuds (ou les deux) n'existe pas. Créez-les d'abord !")
-
+            
     def run_dfs(self):
         """Lance DFS et visualise le résultat."""
         # TODO: implémenter
