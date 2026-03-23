4dfb8f5c8ed9f32e73455ef0acbe40e1ca41148b
NlsHub <nilscoudry@gmail.com>
2026-03-23 09:36:08 +0100

    Update app.py

--- src/app/ui/app.py
+++ src/app/ui/app.py
@@ -156,11 +156,11 @@ class GraphExplorerApp:
         
         if nom_noeud and nom_noeud.strip():
             nom_noeud = nom_noeud.strip()
-            
             if nom_noeud not in self.graph.nodes():
                 self.graph.add_node(nom_noeud)
                 self.node_listframe.insert(tk.END, nom_noeud)
                 self.statusVariable.set(f"Nœud '{nom_noeud}' ajouté avec succès.")
+                self.draw_graph()
             else:
                 messagebox.showwarning("Attention", f"Le noeud '{nom_noeud}' existe déjà !")
     
