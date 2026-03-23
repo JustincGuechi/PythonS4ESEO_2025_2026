3038efbdc8e31934d2b0bd22f351d55c9a838ace
NlsHub <nilscoudry@gmail.com>
2026-03-23 08:35:42 +0100

    Update controller.py

--- src/app/ui/controller.py
+++ src/app/ui/controller.py
@@ -43,16 +43,7 @@ class GraphController:
         """
         # TODO: implémenter
         # Validation + appel à core.algorithms.dfs()
-
-        nodes = self.graph.nodes()
-        
-        if not nodes:
-            raise ValueError("Le graphe est vide, impossible de lancer un parcours.")
-
-        if start not in nodes:
-            raise ValueError("Le nœud de départ n'existe pas dans le graphe.")
-            
-        return dfs(self.graph, start)
+        pass
     
     def execute_bfs(self, start: str) -> list[str]:
         """
