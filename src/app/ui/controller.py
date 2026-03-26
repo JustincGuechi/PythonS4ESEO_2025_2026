7974c2f35469b2f1b01b9036787660a4858cf4aa
NlsHub <nilscoudry@gmail.com>
2026-03-26 08:58:18 +0100

    Update controller.py

--- src/app/ui/controller.py
+++ src/app/ui/controller.py
@@ -66,15 +66,18 @@ class GraphController:
         # TODO: implémenter
 
         if start not in self.graph.nodes():
-            raise ValueError(f"Noeud {start} n'existe pas.")
+            raise ValueError("le graph est vide")
+
+        if start not in noeuds:
+            raise ValueError(f"Noeud '{start}' n'existe pas.")
 
         try:
-            ordre_visite=core.algorithms.bfs(self.graph,start)
+            ordre_visite = bfs(self.graph, start)
             return ordre_visite
-
+            
         except Exception as e:
-            raise ValueError("Erreur lors de l'exécution de la DFS")
-    
+            raise ValueError(f"Erreur lors de l'exécution de la BFS : {e}")
+
     def find_shortest_path(self, start: str, goal: str) -> list[str] | None:
         """
         Trouve le plus court chemin entre deux nœuds.
