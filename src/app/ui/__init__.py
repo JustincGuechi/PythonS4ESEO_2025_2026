3e2441273955f6c09b7951567e138e6411161adb
NlsHub <nilscoudry@gmail.com>
2026-04-14 12:02:21 +0200

    Update __init__.py

--- src/app/ui/__init__.py
+++ src/app/ui/__init__.py
@@ -6,7 +6,7 @@ Interface graphique de l'application.
 
 from .app import GraphExplorerApp, main
 from .controller import GraphController
-from .render import draw_graph, highlight_path, auto_layout
+from .render import draw_graph, highlight_path
 
 __all__ = [
     "GraphExplorerApp",
